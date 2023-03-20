bd301cb579e967cf460b8e991009f5ae614ee01a
maksat607 <kuleshov.chyngyz@gmail.com>
2023-03-20 23:19:22 +0600

    adding webhook

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ApplicationChat;
+use App\Models\Customer;
+use App\Notifications\UserNotifications;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Storage;
@@ -12,13 +15,32 @@ class TelegramController extends Controller
 {
     public function handle(Request $request)
     {
-        Storage::disk('local')->put('example.txt', json_encode($request->all()));
+        Storage::disk('local')->append('example.txt', json_encode($request->all()));
         $message = $request->input('message.text');
         $chatId = $request->input('message.chat.id');
         $first_name = $request->input('message.chat.first_name');
         $last_name = $request->input('message.chat.last_name');
         $username = $request->input('message.chat.username');
         Storage::disk('local')->put('fullname.txt', $message.' '.$chatId.' '.$first_name.' '.$last_name.' '.$username);
+
+
+        $customer = Customer::firstOrCreate([
+            'telegram_id'=>$chatId,
+            'fullname'=>$first_name.' '.$last_name,
+            'telegram_id'=>$chatId,
+            'username'=>$username,
+        ]);
+
+
+        $data = [
+            'user_id'=> 0,
+            'curomer_id'=>$customer->id,
+            'message'=>$message,
+            'self'=>1
+        ];
+        $customer->notify(new UserNotifications($data));
+        $customer->load('notifications');
+        event(new ApplicationChat($customer,$data));
 
 //        $client = new Client(['base_uri' => 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/']);
 //        $client->post('sendMessage', [
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::middleware('auth')->group(function () {
         return redirect('customer/1/chat');
     })->middleware(['verified'])->name('dashboard');
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
