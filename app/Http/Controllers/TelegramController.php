0480dba10d6de9fadc18bcd25da9889b7543262d
maksat607 <kuleshov.chyngyz@gmail.com>
2023-03-20 22:23:02 +0600

    adding webhook

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -16,13 +16,13 @@ class TelegramController extends Controller
 //        $message = $request->input('message.text');
 //        $chatId = $request->input('message.chat.id');
 //
-        $client = new Client(['base_uri' => 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/']);
-        $client->post('sendMessage', [
-            'json' => [
-                'chat_id' => '$chatId',
-                'text' => 'You said: ' . '$message',
-            ],
-        ]);
+//        $client = new Client(['base_uri' => 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/']);
+//        $client->post('sendMessage', [
+//            'json' => [
+//                'chat_id' => '$chatId',
+//                'text' => 'You said: ' . '$message',
+//            ],
+//        ]);
 
         return 'OK';
     }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/telegram-bot', [\App\Http\Controllers\TelegramController::class, 'handle']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
