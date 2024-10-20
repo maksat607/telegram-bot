cc9c620ff6f666fdc5c1169c2fe3a53e9c8b9ee6
maksat607 <kuleshov.chyngyz@gmail.com>
2024-10-20 10:52:28 +0600

    use Intervention\Image\Laravel\Facades\Image;

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -8,7 +8,7 @@ use App\Notifications\UserNotifications;
 use App\Services\Telegram;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Laravel\Facades\Image;
+use Intervention\Image\Facades\Image;
 use Telegram\Bot\Api;
 
 
@@ -224,10 +224,9 @@ class TelegramController extends Controller
 
 
         $filename = basename($fileUrl);
-        Image::make(file_get_contents($fileUrl))->resize(300, null, function ($constraint) {
-            $constraint->aspectRatio();
+        $originalImagePath = public_path('/uploads/' . $filename);
+        $this->resizeImage($originalImagePath, public_path('/uploads/thumbnails/' . $filename), 300);
 
-        })->save(public_path() . '/uploads/thumbnails/' . $filename);
         $thumbnail_url = Storage::disk('uploads')->url('thumbnails/' . $filename);
         $url = Storage::disk('uploads')->url($filename);
 
@@ -322,10 +321,13 @@ class TelegramController extends Controller
 
                 $fileName = time() . '_' . $singleFile->getClientOriginalName();
                 Storage::disk('uploads')->put($fileName, file_get_contents($singleFile));
+                $originalImagePath = public_path('/uploads/' . $fileName);
+
                 if (in_array(strtolower($singleFile->getClientOriginalExtension()), ['jpg', 'jpeg', 'png', 'bmp'])) {
-                    Image::make($singleFile->path())->resize(300, null, function ($constraint) {
-                        $constraint->aspectRatio();
-                    })->save(public_path() . '/uploads/thumbnails/' . $fileName);
+//                    Image::make($singleFile->path())->resize(300, null, function ($constraint) {
+//                        $constraint->aspectRatio();
+//                    })->save(public_path() . '/uploads/thumbnails/' . $fileName);
+                    $this->resizeImage($originalImagePath, public_path('/uploads/thumbnails/' . $fileName), 300);
                     $thumbnail_url = Storage::disk('uploads')->url('thumbnails/' . $fileName);
                     $url = Storage::disk('uploads')->url($fileName);
                 } else {
@@ -386,6 +388,29 @@ class TelegramController extends Controller
 
         Telegram::sendMessage($data);
     }
+    protected function resizeImage($filePath, $destinationPath, $width)
+    {
+        // Load the image
+        $image = imagecreatefromjpeg($filePath); // Change this based on your image type
 
+        // Get original dimensions
+        list($originalWidth, $originalHeight) = getimagesize($filePath);
+
+        // Calculate the new height while maintaining aspect ratio
+        $height = ($originalHeight / $originalWidth) * $width;
+
+        // Create a new true color image
+        $newImage = imagecreatetruecolor($width, $height);
+
+        // Resize the image
+        imagecopyresampled($newImage, $image, 0, 0, 0, 0, $width, $height, $originalWidth, $originalHeight);
+
+        // Save the resized image
+        imagejpeg($newImage, $destinationPath, 100); // Change to imagepng for PNG images
+
+        // Free up memory
+        imagedestroy($image);
+        imagedestroy($newImage);
+    }
 
 }
