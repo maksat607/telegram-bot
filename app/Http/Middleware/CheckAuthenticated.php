df25596ab0759390e38cbafa83defe52638e9196
maksat607 <kuleshov.chyngyz@gmail.com>
2024-10-20 17:24:40 +0600

    adding logout

--- app/Http/Middleware/CheckAuthenticated.php
+++ app/Http/Middleware/CheckAuthenticated.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Closure;
 use Illuminate\Http\Request;
@@ -15,7 +16,9 @@ class CheckAuthenticated
      */
     public function handle(Request $request, Closure $next): Response
     {
+        Log::info('Session in middleware:', Session::all());
         if (!Session::has('token')) {
+            Log::info('User is not authenticated');
             // Redirect to login if user is not authenticated
             return redirect()->route('login');
         }
