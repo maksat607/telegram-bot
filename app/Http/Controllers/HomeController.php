135be9ca2cfb5bb129d8f6b5fe6a6878aae2f78e
maksat607 <kuleshov.chyngyz@gmail.com>
2025-01-23 22:56:02 +0600

    adding log

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends Controller
             $customers = Customer::whereHas('notifications', function($query) use ($request) {
                 $query->where('data', 'like', "%$request->search%")
                     ->orWhereNull('data');
-            })->orderBy('created_at', 'desc')->get();
+            })->orderBy('created_at', 'asc')->get();
             return view('ajaxcontent',compact('customers','search'));
         }
         $username = $this->telegram->username();
