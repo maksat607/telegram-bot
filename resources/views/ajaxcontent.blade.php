314625b11a69670a2f5c5efdd2b0ff44eb4d7685
maksat607 <kuleshov.chyngyz@gmail.com>
2025-01-26 09:49:56 +0600

    adding new login with token

--- resources/views/ajaxcontent.blade.php
+++ resources/views/ajaxcontent.blade.php
@@ -1,6 +1,6 @@
 
-@foreach($customers as $customer)
-    <div id="customer-{{$customer->id}}">
-        @include('customer')
-    </div>
-@endforeach
+{{--@foreach($customers as $customer)--}}
+{{--    <div id="customer-{{$customer->id}}">--}}
+{{--        @include('customer')--}}
+{{--    </div>--}}
+{{--@endforeach--}}
