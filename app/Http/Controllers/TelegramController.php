344f07399f4aa1bf097a892f24e164498761bd4c
maksat607 <kuleshov.chyngyz@gmail.com>
2025-07-21 18:14:44 +0600

    asdfdasf

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -70,7 +70,7 @@ class TelegramController extends Controller
         switch (true) {
             case isset($dataR['message']['contact']['phone_number']):
                 Log::info('Phone number shared', ['phone_number' => $dataR['message']['contact']['phone_number']]);
-                return $this->handleContact($request, $dataR);
+                return $this->handleContact($request, $chatId);
             case isset($dataR['message']['voice']):
                 Log::info('Voice');
                 return $this->handleVoice($request, $dataR);
