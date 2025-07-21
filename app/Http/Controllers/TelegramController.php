f4812774d4ec5618879cda769bf811e8a24b9a66
maksat607 <kuleshov.chyngyz@gmail.com>
2025-07-21 17:20:19 +0600

    asdfdasf

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -64,7 +64,7 @@ class TelegramController extends Controller
     public function handle(Request $request, Telegram $telegram)
     {
         extract($this->getInfo($request));
-        $this->requestContactForFeatures($request, $telegram);
+        $this->requestContactForFeature();
         $dataR = json_decode($request->getContent(), true);
         Storage::disk('local')->append('json.txt', json_encode(($dataR)));
 
